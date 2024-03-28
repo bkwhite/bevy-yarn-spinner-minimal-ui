33e9d57364b8261ddff71f5bad469fdd118bfbd3
bkwhite <bleywhite@gmail.com>
2024-03-28 10:51:54 -0400

    [tutorial] block 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+//! This example illustrates how to create a button that changes color and text based on its
+//! interaction state.
+
+use bevy::prelude::*;
+use bevy_yarnspinner::events::PresentLineEvent;
+
+#[derive(Resource, Default)]
+pub struct SpinnerCurrentLine(pub String);
+
+#[derive(Component, Default)]
+pub struct DialogLine;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .init_resource::<SpinnerCurrentLine>()
+        .add_systems(Startup, setup)
+        .add_systems(Update, present_line.run_if(on_event::<PresentLineEvent>()))
+        .run();
+}
+
+fn setup(asset_server: Res<AssetServer>, mut commands: Commands) {
+   commands
+        .spawn(NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn((
+                TextBundle::from_section(
+                    "waiting for dialog...",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 100.0,
+                        ..default()
+                    },
+                )
+                .with_text_justify(JustifyText::Center),
+                DialogLine,
+            ));
+        });
+}
+
+fn present_line(mut reader: EventReader<PresentLineEvent>) {
+    for evt in reader.read() {}
 }
